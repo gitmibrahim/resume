6d84259e450c73d67b7132d28b26349911557f1c
Muhammad Ibrahim <engineermohamed.ibrahim.mi@gmail.com>
2018-11-18 14:48:38 +0200

    initial building configurations

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './components/index.js',
+    output: {
+        path: path.resolve(__dirname, 'docs'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'},
+            { test: /\.(sass|scss)$/, use: ["style-loader", "css-loader", "sass-loader"] }
+        ]
+    },
+    mode: 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/index.html'
+        })
+    ]
+};
