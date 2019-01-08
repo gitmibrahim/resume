020d117fa343198fb04b690686fe6ec16ba0cddf
Muhammad Ibrahim <engineermohamed.ibrahim.mi@gmail.com>
2019-01-08 15:09:00 +0200

    relative path problem v1.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,19 @@
 var path = require('path')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './components/index.jsx',
     output: {
         path: path.resolve(__dirname, 'docs'),
+        publicPath: './assets/',
         chunkFilename: `[id].js`
     },
     module: {
         rules: [
-            { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader'},
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, use: 'babel-loader' },
             { test: /\.css$/, use: ["style-loader", "css-loader"] },
-            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
+            { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader' },
             { test: /\.(sass|scss)$/, use: ["style-loader", "css-loader", "sass-loader"] }
         ]
     },
@@ -19,6 +21,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './components/index.html'
-        })
+        }),
+        new CopyWebpackPlugin([
+            { from: './assets', to: 'assets' }
+        ]),
     ]
 };
